25f1e6e4e1b72031615a14f840c1f58e3deea2c2
VikashTiwari23 <vikash.tiwari.236968@gmail.com>
2026-03-27 15:18:12 +0530

    Update Medium.java

--- Array/Medium.java
+++ Array/Medium.java
@@ -146,6 +146,19 @@ public class Main {
         return true;    
     }
   
+  public int firstMissingPositive(int[] nums) {
+        Set<Integer>set = new HashSet<>();
+        
+        for(int num:nums){
+            if(num>0) set.add(num);
+        }
+        int n= nums.length;
+        for(int  i=1;i<=nums.length;i++){
+            if(!set.contains(i))return i;
+        }
+        return n+1;
+ }
+
   
     public static void main(String[] args) {
       
